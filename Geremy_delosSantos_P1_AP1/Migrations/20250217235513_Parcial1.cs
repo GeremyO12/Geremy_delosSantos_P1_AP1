d28a62ab0e7c0449e7eab8a95bfd159f901b368d
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2025-02-17 21:07:21 -0400

    Se creo la migracion

---
+++ Geremy_delosSantos_P1_AP1/Migrations/20250217235513_Parcial1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Geremy_delosSantos_P1_AP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Parcial1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aporte",
+                columns: table => new
+                {
+                    AporteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Persona = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Observacion = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Monto = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aporte", x => x.AporteId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aporte");
+        }
+    }
+}
